d9880dc6fb66559443911dd2bcb460253b74f00c
adiadi226 <adrian22626@gmail.com>
2018-04-12 22:44:19 +0200

    Pierwsza wersja!

---
+++ TestProject/TestowaKlasa.cpp
@@ -0,0 +1,18 @@
+#include "TestowaKlasa.h"
+
+int TestowaKlasa::JakasPrywatna(double y)
+{
+	return (int)(y*y*y);
+}
+
+TestowaKlasa::TestowaKlasa(int g)
+{
+	int h = g * g;
+}
+
+void TestowaKlasa::JakasPublicznaMetoda(int x)
+{
+	int g = 0;
+	g += x;
+	g = g * g;
+}
---
+++ TestProject/TestowaKlasa.h
@@ -0,0 +1,16 @@
+#ifndef TestowaKlasa
+
+
+class TestowaKlasa
+{
+	private:
+		int JakasPrywatna(double y);
+
+	public:
+		TestowaKlasa(int g);
+		void JakasPublicznaMetoda(int x);
+};
+
+
+#endif
+class TestowaKlasa;
